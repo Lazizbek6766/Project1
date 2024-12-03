1a01561a665493429333556e1e814a03107356f2
Lazizbek <turayevlazizbek6766@gmail.com>
2024-12-03 09:39:58 +0500

    Initial Commit

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Mac on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
